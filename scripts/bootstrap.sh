67adf1a48be69b6ddd7539b5c63327e1ea58f529
tronje <hi@tron.je>
2017-12-02 15:16:50 +0100

    Get bootstrap script working properly

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,23 +3,21 @@
 USER=$(whoami)
 CONTAINER=ubuntu
 
-# grab root
-sudo su
+# grab mkosi in case we don't have it
+pacaur -S mkosi --noconfirm --noedit --needed
+pacaur -S debootstrap ubuntu-keyring --asdeps --noconfirm --noedit --needed
 
-# this is where nspawn containers live
+# execute the rest of the script as root
+exec sudo -i /bin/sh - << EOF
+
+# this is where the machines live
 cd /var/lib/machines
 
 # if the machine already exists, do nothing
-if [ -d $CONTAINER ]; then
-    echo "Machine 'ubuntu' exists! Aborting..."
-    exit 1
+if [ ! -d $CONTAINER ]; then
+    # bootstrap ubuntu xenial with mkosi
+    mkosi -d ubuntu -r xenial --repositories main,universe -t directory -o $CONTAINER
 fi
-
-# grab mkosi in case we don't have it
-pacaur -S mkosi --noconfirm --noedit --needed
-
-# bootstrap ubuntu xenial with mkosi
-mkosi -d ubuntu -r xenial --repositories main,universe -t directory -o $CONTAINER
 
 # to run Ubuntu in nspawn, we need to remove makedev, and install sudo
 systemd-nspawn -D $CONTAINER /usr/bin/apt-get -y remove makedev
@@ -37,15 +35,19 @@ systemd-nspawn -D $CONTAINER /bin/systemctl enable systemd-networkd
 # we'll also add a user...
 systemd-nspawn -D $CONTAINER /usr/sbin/useradd -m -G sudo $USER
 
-# basic config for this machine
-cat > /etc/systemd/nspawn/$CONTAINER.nspawn << EOF
-[Exec]
-Boot=1
+if [ ! -e /etc/systemd/nspawn/$CONTAINER.nspawn ]; then
+    # basic config for this machine
+    cat > /etc/systemd/nspawn/$CONTAINER.nspawn << EOM
+    [Exec]
+    Boot=1
+    
+    [Files]
+    Bind=/run/user/1000/pulse:/run/user/host/pulse
+    Bind=/home/$USER/thesis/data:/home/$USER/data
+    
+    [Network]
+    VirtualEthernet=0
+    EOM
+fi
 
-[Files]
-Bind=/run/user/1000/pulse:/run/user/host/pulse
-Bind=/home/$USER/thesis/data:/home/$USER/data
-
-[Network]
-VirtualEthernet=0
 EOF
