98c8e5a19a297f61f81f50f341cd9b45f5f699ca
tronje <hi@tron.je>
2017-12-23 18:10:59 +0100

    Add some data and tweak the python source

--- src/analyze.py
+++ src/analyze.py
@@ -19,11 +19,68 @@ def analyze(metric, dbpath):
                   ON site_visits.visit_id = javascript.visit_id;
             """)
 
+    sites = {}
+    for i in range(1, num_visits + 1):
+        sites[i] = {
+                'url': None,
+                'font_count': 0,
+                'measure_count': 0,
+                'to_data_url': False,
+                'get_image_data': False,
+                'create_data_channel': False,
+                'create_offer': False,
+                'onicecandidate': False,
+                'fp_font': False,
+                'fp_canvas': False,
+                'fp_webrtc': False,
+                }
+
+
     for entry in cursor.fetchall():
-        if "Canvas" in entry[2]:
-            print(f"OMG FINGERPRINTER! {entry[1]}")
+        _id = entry[0]
+        _url = entry[1]
+        _symbol = entry[2]
+
+        if sites[_id]['url'] is None:
+            sites[_id]['url'] = _url
+
+        if "font" in entry[2]:
+            sites[_id]['font_count'] += 1
+        if "measureText" in entry[2]:
+            sites[_id]['measure_count'] += 1
+        if "toDataURL" in entry[2]:
+            sites[_id]['to_data_url'] = True
+        if "getImageData" in entry[2]:
+            sites[_id]['get_image_data'] = True
+        if "createDataChannel" in entry[2]:
+            sites[_id]['create_data_channel'] = True
+        if "createOffer" in entry[2]:
+            sites[_id]['create_offer'] = True
+        if "onicecandidate" in entry[2]:
+            sites[_id]['onicecandidate'] = True
 
     conn.close()
+
+
+    for _id in sites.keys():
+        if sites[_id]['font_count'] >= 50 and sites[_id]['measure_count'] >= 50:
+            sites[_id]['fp_font'] = True
+
+        if sites[_id]['to_data_url'] and sites[_id]['get_image_data']:
+            sites[_id]['fp_canvas'] = True
+
+        if sites[_id]['create_data_channel'] and sites[_id]['create_offer'] \
+                and sites[_id]['onicecandidate']:
+            sites[_id]['fp_webrtc'] = True
+
+
+    for _id in sites.keys():
+        if sites[_id]['fp_font'] or sites[_id]['fp_canvas'] or sites[_id]['fp_webrtc']:
+            print(f"""{sites[_id]['url']}
+                    font: {sites[_id]['fp_font']}
+                    canvas: {sites[_id]['fp_canvas']}
+                    webrtc: {sites[_id]['fp_webrtc']}""")
+            print()
 
 
 if __name__ == "__main__":
